33d27f751ac53567dca30c002085f1de4d462a0f
olive <olivealavine@gmail.com>
2022-05-02 18:04:26 -0500

    wave 4

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,24 @@
+from attr import validate
 from app import db
 from app.models.book import Book
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request, abort
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+# helper functions
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+    
+    book = Book.query.get(book_id)
+    if not book:
+        abort(make_response({"message":f"book {book_id} not found"}, 404))
+    
+    return book
+
+# route functions
 @books_bp.route("", methods=["GET"])
 def read_all_books():
     books = Book.query.all()
@@ -15,6 +30,16 @@ def read_all_books():
             "description": book.description
         })
     return jsonify(books_response)
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def read_one_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
 
 @books_bp.route("", methods=["POST"])
 def create_book():
@@ -30,41 +55,26 @@ def create_book():
 
     return make_response(f"Book {new_book.title} successfully created", 201)
 
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id)
+    request_body = request.get_json()
 
-
-
-
-
-
-
-# @books_bp.route("", methods=["GET"])
-# def handle_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append({
-#             "id": book.id,
-#             "title": book.title,
-#             "description": book.description
-#         })
-#     return jsonify(books_response)
-
-# def validate_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except:
-#         abort(make_response({"message":f"book {book_id} invalid"}, 400))
+    if "title" not in request_body or "description" not in request_body:
+        return make_response("Invalid Request", 400)
     
-#     for book in books:
-#         if book.id == book_id:
-#             return book
-#     abort(make_response({"message":f"book {book_id} not found"}, 404))
+    book.title = request_body["title"]
+    book.description = request_body["description"]
 
-# @books_bp.route("/<book_id>", methods=["GET"])
-# def handle_book(book_id):
-#     book = validate_book(book_id)
+    db.session.commit()
 
-#     return{
-#         "id": book.id,
-#         "title": book.title,
-#         "description": book.description
-#     }
+    return f"Book #{book_id} successfully updated"
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return make_response(f"Book {book_id} successfully deleted")
