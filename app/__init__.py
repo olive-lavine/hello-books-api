cddbb4f00121d7b43006e747d9ccca2a2b7e9120
olive <olivealavine@gmail.com>
2022-05-10 21:15:05 -0500

    completed author model and routes

--- app/__init__.py
+++ app/__init__.py
@@ -21,12 +21,18 @@ def create_app(test_config=None):
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_TEST_DATABASE_URI")
 
+    # Import models here
+    from app.models.book import Book
+    from app.models.author import Author
+
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from app.models.book import Book
-
-    from .routes import books_bp
+    # Register Blueprints here
+    from .book_routes import books_bp
     app.register_blueprint(books_bp)
+
+    from .author_routes import authors_bp
+    app.register_blueprint(authors_bp)
 
     return app
